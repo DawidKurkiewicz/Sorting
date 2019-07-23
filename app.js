2e0c1fa8fe050a5cf0612a698b7e38480fb73b09
Dawid Kurkiewicz <dawid.kurkiewicz@billennium.com>
2019-07-23 09:28:44 +0200

    fix

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'N
 function strip(bandName){
     return bandName.replace(/^(a |the |an )/i, '').trim();
 }
+
 const sortedBands = bands.sort((a, b)=> {
     if (strip(a) > strip(b)) {
         return 1;
